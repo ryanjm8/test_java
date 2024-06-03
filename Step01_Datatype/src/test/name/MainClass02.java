4fdd5c99e90a22492c5468f6ec6a7835d616be06
leejeongmin <abcdiue@nate.com>
2024-06-03 14:56:40 +0900

    커밋추가함

---
+++ Step01_Datatype/src/test/name/MainClass02.java
@@ -0,0 +1,17 @@
+package test.name;
+
+public class MainClass02 {
+	public static void main(String[] args) {
+		//1. 콘솔창에 "main" 메소드가 시작되었습니다. 출력해보세요.
+		System.out.println("main 메소드가 시작되었습니다.");
+		//2. 국어점수 kor을 kor이라는 변수에 담아보세요.
+		int kor=95;
+		//3. 영어  eng을 kor이라는 변수에 담아보세요.
+		int eng=100;
+		//4. 국어점수와 영어점수의 평균점수를 구해서 avg 변수에담아보세요
+		double avg= (kor+eng)/2d ;
+		//5. 콘솔창에 avg 변수에 담긴 내용을 출력해보세요.
+		System.out.println(avg);
+				
+	}
+}
